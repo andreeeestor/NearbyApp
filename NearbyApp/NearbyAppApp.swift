d3c938ab01ad2ca7e28d3f8b25283edfa22d362e
andreeeestor <andrenestor0503@gmail.com>
2025-03-29 18:09:29 -0300

    Initial Commit

---
+++ NearbyApp/NearbyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyAppApp.swift
+//  NearbyApp
+//
+//  Created by André Nestor on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NearbyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
